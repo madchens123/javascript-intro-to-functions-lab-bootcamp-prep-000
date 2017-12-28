f5ab251ddd580fd8a0d160e8c338454ab7933037
Ryland Bowles <ryland.bowles@comcast.net>
2017-12-28 02:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return ("I can't hear you!")
-  return ("YES INDEED!")
+  if string = return ("YES INDEED!")
   return ("I love you, too.")
 }
 
