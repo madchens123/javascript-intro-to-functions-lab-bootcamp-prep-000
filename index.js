142120faf22676e3a1c6a51fcf04a890b2a45013
Ryland Bowles <ryland.bowles@comcast.net>
2017-12-28 02:56:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return ("I can't hear you!")
+  
+}
+
+function sayHiToGrandma(string) {
   return ("YES INDEED!")
+}
+
+function sayHiToGrandma(string) {
   return ("I love you, too.")
 }
 
